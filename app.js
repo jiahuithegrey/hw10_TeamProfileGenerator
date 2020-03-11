6f6769a7fbd1a8fe07e38f05d3af4980d97cd7a3
Jiahui Wang <wjh4289042@hotmail.com>
2020-03-11 16:34:07 -0700

    add html

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateHtml = require("./lib/generateHtml.js");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const manager = require("./lib/Manager.js");
 const engineer = require("./lib/Engineer.js");
 const intern = require("./lib/Intern.js");
 const questions = require("./lib/Questions.js");
-const generateHtml = require("./lib/generateHtml.js");
 
 // const path = require("path");
 // const outputPath = path.resolve(__dirname, "output", "team.html");
@@ -47,7 +47,7 @@ function getManager(ques) {
       );
       teamArr.push(manager);
 
-      writeToHTML(managerInfo);
+      writeToHTML(manager);
       init(); //start a new round of questions
     },
     function(err) {
@@ -56,41 +56,77 @@ function getManager(ques) {
   );
 }
 
-const generateHTML = require("./lib/generateMainHTML.js");
-
-function writeToHTML(res) {
-  let html = generateHTML(res);
-  writeFileAsync(`${teamName}_Profile.html`, html);
-  //console.log("Successfully wrote to html!");
-}
 function getEngineer(ques) {
-  inquirer.prompt(ques).then(
-      function(res) {
-        const engineer = new engineer(
-            res.engineerName,
-            res.engineerID,
-            res.engineerEmail,
-            res.repoName
-        );
+  inquirer.prompt(ques).then(function(res) {
+    const engineer = new engineer(
+      res.engineerName,
+      res.engineerID,
+      res.engineerEmail,
+      res.repoName
+    );
     teamArr.push(engineer);
 
-    writeToHTML(engineerInfo);
+    writeToHTML(engineer);
     init();
   });
 }
 
-function getIntern(ques){
-    inquirer.prompt(ques).then(
-        function(res){
-            const intern = new intern(
-                res.internName,
-                res.internID,
-                res.internEmail,
-                res.repoName
-            );
-        teamArr.push(intern);
-        
-        writeToHTML(internInfo);
-        init();
-    });
+function getIntern(ques) {
+  inquirer.prompt(ques).then(function(res) {
+    const intern = new intern(
+      res.internName,
+      res.internID,
+      res.internEmail,
+      res.repoName
+    );
+    teamArr.push(intern);
+
+    writeToHTML(intern);
+    init();
+  });
+}
+
+let html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+      <meta charset="UTF-8">
+      <title>Team</title>
+      <!-- Latest compiled and minified CSS & JS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="https://code.jquery.com/jquery.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      <script src="https://kit.fontawesome.com/4d07055d3e.js" crossorigin=“anonymous”></script>
+      <style>
+          .shadow {
+              box-shadow: 5px 5px 5px grey;
+          }
+      </style>
+  </head>
+  <body>
+      <div class="container-fluid p-0 mb-0">
+          <div class="jumbotron jumbotron-fluid bg-danger text-light">
+              <div class="container text-center">
+                  <h1 class="display-4">My Team</h1>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row justify-content-center" id="cards">
+  `;
+
+let close = `
+        </div> 
+        </div> 
+      </body>
+    </html>;`;
+
+function writeToHTML() {
+  for (let i = 0; i < teamArr.length; i++) {
+    const card = generateHtml(teamArr[i]);
+    html += card;
+  }
+  html += close;
+  console.log("Successfully wrote to html!");
+  writeFileAsync(`./output/${teamName}_Profile.html`, html);
 }
