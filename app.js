319eed5b6926b6a3a5f0872917a4a787dbcce0b5
Jiahui Wang <wjh4289042@hotmail.com>
2020-03-11 16:20:21 -0700

    updates

--- app.js
+++ app.js
@@ -1,106 +1,96 @@
-const manager = require ("./lib/Manager");
-const engineer = require ("./lib/Engineer");
-const intern = require ("./lib/Intern");
-const questions = require("./lib/Questions");
-const inquirer = require ("inquirer");
-const fs = require ("fs");
-const util = require ("util");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-//when to use const when to use let?????
-//how to make a cirle till user chooses done?
-//ask user their role is
+const manager = require("./lib/Manager.js");
+const engineer = require("./lib/Engineer.js");
+const intern = require("./lib/Intern.js");
+const questions = require("./lib/Questions.js");
+const generateHtml = require("./lib/generateHtml.js");
+
+// const path = require("path");
+// const outputPath = path.resolve(__dirname, "output", "team.html");
+// const render = require("./lib/htmlRenderer");
+
 init();
 
-function init(){
-inquirer
-    .prompt(questions.titleQuestion)
-    .then (function(res){
-        if (res === "Manager"){
-            getManager(questions.managerQuestions);
-        }
-        if (res === "Engineer"){
-            getEngineer(questions.engineerQuestions);
-        }
-        if (res === "Intern"){
-            getIntern(questions.internQuestions);
-        }
-        if (res === "Done"){
-            writeToHTML();
-        }
-    });
-} 
+function init() {
+  inquirer.prompt(questions.titleQues).then(function(res) {
+    if (res.title === "Manager") {
+      getManager(questions.managerQues);
+    }
+    if (res.title === "Engineer") {
+      getEngineer(questions.engineerQues);
+    }
+    if (res.title === "Intern") {
+      getIntern(questions.internQues);
+    }
+    if (res.title === "Done") {
+      writeToHTML();
+    }
+  });
+}
 
+const teamArr = [];
 let teamName;
 
-function getManager(ques){
-    inquirer
-    .prompt(ques)
-    .then (function(res){
-        teamName = res.teamName;
-        let title = "Manager";
-        let managerName = res.managerName;
-        let managerID = res. managerID;
-        let managerEmail = res. managerEmail;
-        let managerRoomNo = res.managerRoomNumber;
+function getManager(ques) {
+  inquirer.prompt(ques).then(
+    function(res) {
+      teamName = res.teamName;
+      const manager = new Manager(
+        res.managerName,
+        res.managerID,
+        res.managerEmail,
+        res.managerRmNum
+      );
+      teamArr.push(manager);
 
-        let managerInfo = {
-            team: teamName,
-            title: title,
-            name: managerName,
-            id: managerID,
-            email: managerEmail,
-            room: managerRoomNo
-        }
-        writeToHTML(managerInfo);
-        init(); //start a new round of questions
-    }, function (err){
-        console.log(err);
-    });
+      writeToHTML(managerInfo);
+      init(); //start a new round of questions
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
 }
 
 const generateHTML = require("./lib/generateMainHTML.js");
 
-function writeToHTML(res){
-    let html = generateHTML(res);
-    writeFileAsync(`${teamName}_Profile.html`,html);
-    //console.log("Successfully wrote to html!");
+function writeToHTML(res) {
+  let html = generateHTML(res);
+  writeFileAsync(`${teamName}_Profile.html`, html);
+  //console.log("Successfully wrote to html!");
 }
-// function getEngineer(ques){
-//     inquirer
-//     .prompt(ques)
-//     .then (function(res){
-//         let engineerName = res.engineerName;
-//         let engineerID = res. engineerID;
-//         let engineerEmail = res. engineerEmail;
-//         let repoName = res.repoName;
+function getEngineer(ques) {
+  inquirer.prompt(ques).then(
+      function(res) {
+        const engineer = new engineer(
+            res.engineerName,
+            res.engineerID,
+            res.engineerEmail,
+            res.repoName
+        );
+    teamArr.push(engineer);
 
-//         let engineerInfo = {
-//             name: engineerName,
-//             id: engineerID,
-//             email: engineerEmail,
-//             repo: repoName
-//         }
-//         writeToHTML(engineerInfo);
-//         init();
-//     });
-// }
-// function getIntern(ques){
-//     inquirer
-//     .prompt(ques)
-//     .then (function(res){
-//         let internName = res.internName;
-//         let internID = res. internID;
-//         let internEmail = res. internEmail;
-//         let internSchool = res.internSchool;
+    writeToHTML(engineerInfo);
+    init();
+  });
+}
 
-//         let internInfo = {
-//             name: internName,
-//             id: internID,
-//             email: internEmail,
-//             repo: internName
-//         }
-//         writeToHTML(internInfo);
-//         init();
-//     });
-// }
+function getIntern(ques){
+    inquirer.prompt(ques).then(
+        function(res){
+            const intern = new intern(
+                res.internName,
+                res.internID,
+                res.internEmail,
+                res.repoName
+            );
+        teamArr.push(intern);
+        
+        writeToHTML(internInfo);
+        init();
+    });
+}
